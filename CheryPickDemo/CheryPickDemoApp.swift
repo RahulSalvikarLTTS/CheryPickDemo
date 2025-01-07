0f160d9b415791751d76b74a1959ecf490cf16c4
RahulSalvikarLTTS <rahul.salvikar@ltts.com>
2025-01-07 10:54:27 +0530

    Initial Commit

---
+++ CheryPickDemo/CheryPickDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheryPickDemoApp.swift
+//  CheryPickDemo
+//
+//  Created by Rahul Vijayrao Salvikar on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CheryPickDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
